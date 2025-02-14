0321e93ce30d74537aaf02749c6f2b48efed239d
sedorixx <Franziskus.Braun@web.de>
2025-02-14 23:48:54 +0000

    Bereinige Build-Skript: entferne explizite Python-Installation und setze Standard-Python-Version nicht mehr fest

--- build.sh
+++ build.sh
@@ -3,32 +3,6 @@
 echo "PDF Table Extractor - Koyeb Build Script"
 echo "======================================"
 
-# Python-Umgebungsvariablen korrigieren
-unset PYTHONPATH
-unset PYTHONHOME
-
-# Python 3.9 explizit installieren
-curl -O https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
-tar -xzf Python-3.9.16.tgz
-cd Python-3.9.16
-./configure --enable-optimizations
-make -j $(nproc)
-make install
-cd ..
-rm -rf Python-3.9.16*
-
-# Setze Python 3.9 als Standard
-update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.9 1
-
-# Erstelle benötigte Verzeichnisse mit korrekten Rechten
-mkdir -p $HOME/.local/bin
-export PATH="$HOME/.local/bin:$PATH"
-
-# Erstelle virtuelle Umgebung
-python3.9 -m venv venv
-
-# Aktiviere virtuelle Umgebung
-source venv/bin/activate
 
 # Installiere pip und Abhängigkeiten
 pip install -r requirements.txt
